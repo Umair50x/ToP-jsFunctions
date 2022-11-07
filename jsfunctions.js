e4e6b36fa19b018a40b90dd93233dcb999d157ca
Umair50x <i2u1@outlook.com>
2022-11-07 23:14:53 +0500

    four new functions

--- jsfunctions.js
+++ jsfunctions.js
@@ -1,6 +1,22 @@
 
 
+function add7 (a) {
+    return a + 7;
+  }
 
+
+  function multiply(a, b) {
+    return a * b; 
+  }
+
+
+  function capitalize() {
+    
+  }
+
+  function lastletter() {
+    
+  }
 
 
 
