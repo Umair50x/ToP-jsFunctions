a951b297268572c5cb9c775856ad20b5b73fa6cb
Umair50x <i2u1@outlook.com>
2022-11-10 00:05:03 +0500

    function add7(), multiply(), capitalize() & lastletter(): function add7 with concat a "7" with an input number, function multiply returns a product of two numbers, func capitalize & lastletter has been only been given comment instructions for later development.

--- jsfunctions.js
+++ jsfunctions.js
@@ -1,27 +1,42 @@
 
 
+
 function add7 (a) {
-    return a + 7;
-  }
+  return a + " + 7";
+}
+console.log(add7(1));
+
+function multiply(a, b) {
+  return a * b; 
+}
+console.log(multiply(2,5));
 
 
-  function multiply(a, b) {
-    return a * b; 
-  }
-
-
-  function capitalize() {
-    
-  }
-
-  function lastletter() {
-    
-  }
-
-
-
-
+function capitalize() {
+  //prompt the user for a string and save it
+  //convert the whole string first into toLowerCase()
+  //take out the 1st character from this new string using charAt()
+  //capitalize it using toUpperCase()
+  //concat() it to it's original string //can use plus sign too return the result
+}
 
 
 
 
+
+
+
+
+// function lastletter() {
+    //prompt for a string and save it
+    //take out the 1st character using charAt() & return this single character
+// }
+
+
+
+
+// function capitalize() {
+//   getString = prompt("write a string");
+//   return getString.toUpperCase();
+// }
+// alert(capitalize());
