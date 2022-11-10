d8d461c54c88b0005e478d22f8bbf2ee206df7f0
Umair50x <i2u1@outlook.com>
2022-11-10 21:18:45 +0500

    add function capitalize() and lastletter(): capitalize takes all normal string input and returns the string with only the first character capitalized. lastletter slices out the lastletter of the input string.

--- jsfunctions.js
+++ jsfunctions.js
@@ -1,42 +1,37 @@
 
 
 
-function add7 (a) {
-  return a + " + 7";
-}
-console.log(add7(1));
+  function add7 (a) {
+    return a + " + 7";
+  }
+  console.log(add7(1));
 
-function multiply(a, b) {
-  return a * b; 
-}
-console.log(multiply(2,5));
-
-
-function capitalize() {
-  //prompt the user for a string and save it
-  //convert the whole string first into toLowerCase()
-  //take out the 1st character from this new string using charAt()
-  //capitalize it using toUpperCase()
-  //concat() it to it's original string //can use plus sign too return the result
-}
+  function multiply(a, b) {
+    return a * b; 
+  }
+  console.log(multiply(2,5));
 
 
+  function capitalize() {
+    //prompt the user for a string and save it
+    let getString = prompt("type your string.");
+    //convert the whole string first into toLowerCase()
+    let lowered = getString.toLowerCase();
+    //take out the whole string excecpt the first character
+    let shortStr = lowered.slice(1);
+    //take out the 1st character from the lowered string using charAt()//capitalize it using toUpperCase()
+    let fstUpper = lowered.charAt(0).toUpperCase();
+    //concat it to the sliced string
+    return fstUpper + shortStr;
+  }
+  console.log(capitalize());
 
 
-
-
-
-
-// function lastletter() {
-    //prompt for a string and save it
-    //take out the 1st character using charAt() & return this single character
-// }
-
-
-
-
-// function capitalize() {
-//   getString = prompt("write a string");
-//   return getString.toUpperCase();
-// }
-// alert(capitalize());
+  function lastletter() {
+      // prompt for a string and save it
+      let getString = prompt("type a string.");
+      // take out the 1st character using charAt() & return this single character
+      let laststr = getString.slice(-1);
+      return laststr;
+  }
+  console.log("the last letter in your string was letter" + " " + lastletter());
